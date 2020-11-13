020680c2679e822f5982ff2c7e08b2c29a768074
skrngr <grcb@protonmail.com>
2020-11-13 05:59:56 -0600

    returns name, desc, and styles of a given url

---
+++ index.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+
+const url = "https://www.supremenewyork.com/shop/shirts/zkmt62fz1";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+
+  // "networkidle2" option tells browser that the navigation is not finished until there are 2 or less network connections for at least half of a second
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // runs as if it was coming from the browser console
+  const item = await page.evaluate(() => {
+    // retrieve name
+    const name = document.querySelector("h2[class='protect']").innerText;
+
+    // retrieve description
+    const desc = document.querySelector("#details>p:nth-child(3)").innerText;
+
+    // retrieve different styles (array of colors)
+    const models = [];
+    document
+      .querySelectorAll("#details>ul>li>button:first-child")
+      .forEach(el => {
+        models.push(el.getAttribute("data-style-name"));
+      });
+
+    // return everything into item object
+
+    return {
+      name,
+      desc,
+      models
+    };
+  });
+
+  console.log(item.models);
+
+  await browser.close();
+})();
