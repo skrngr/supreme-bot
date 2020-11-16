fe54f6ce16ef18d7e86715d35b99b1c57829a442
skrngr <grcb@protonmail.com>
2020-11-16 04:48:39 -0600

    Created browser branch.

---
+++ utils/Parse.js
@@ -0,0 +1,12 @@
+const Parse = {
+  cmd: d => {
+    let input = d.replace(/\n/i, "").split(" ");
+    let cmd = input[0];
+    let opt = input[1];
+    let vals = Array.from(input.slice(2));
+
+    return { cmd, opt, vals };
+  }
+};
+
+module.exports = Parse;
